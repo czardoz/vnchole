85f0966e36967d40c4ace757d93bac54c1acf3bf
Aniket Panse <aniketpanse@gmail.com>
2013-06-05 00:55:09 +0530

    added mechanism to check challenge-response

--- vnchole.py
+++ vnchole.py
@@ -22,9 +22,33 @@ class RFBDes(pyDes.des):
 
 class VNCDecoder(multiprocessing.Process):
 
-    def __init__(self):
+    def __init__(self, challenge, response, passwd_file='plist.txt'):
         super(VNCDecoder, self).__init__()
-        
+        self.challenge = challenge
+        self.response = response
+        self.passwd_file = passwd_file
+        self.matching_pass = None
+
+    def run(self):
+        with open(self.passwd_file, 'r') as plist:
+            for password in plist:
+                key = (password + '\0' * 8)[:8]
+                encryptor = RFBDes(key)
+                resp = encryptor.encrypt(self.challenge)
+                print 'Trying: ', repr(key), '<==>', repr(response)
+                if resp == self.response:
+                    self.matching_pass = key
+                    print 'Gotcha!'
+
 
 if __name__ == '__main__':
-    pass
+    # challenge = '\xae\xe1`\xab\x97Td\xfc\xbc4Vcr\x87\r\x8e'
+    # response = '*\xa7\x87\xe1\x96dC>)U\xb3\xc6\x03\xcb\x86\xf7'
+
+    challenge = '\x1f\x9c+\t\x14\x03\xfaj\xde\x97p\xe9e\xca\x08\xff'
+    response = '\xe7\xe2\xe2\xa8\x89T\x87\x8d\xf01\x96\x10\xfe\xb9\xc5\xbb'
+
+    d = VNCDecoder(challenge, response)
+    d.start()
+    d.join()
+    print d.matching_pass
